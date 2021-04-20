638b819e30392eeca64a7fca2d821d08ddef9b27
João Guilherme Pessanha Alves <joaopessanhaalves@gmail.com>
2021-04-19 23:45:10 -0300

    db change

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('VAS', 'root', '', {
-    host: "localhost",
+const sequelize = new Sequelize('heroku_f93e64e97944d76', 'b4b98a1c928a5c', '75b06475', {
+    host: "us-cdbr-east-03.cleardb.com",
     dialect: 'mysql'
 });
 
